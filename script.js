05956d084a5d7234239f5f7372a850a0f3d77224
Josh Mendoza <jjm121197@gmail.com>
2021-12-09 10:29:21 -0600

    LS complete

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ let dollarUS = Intl.NumberFormat("en-US", {
     currency: "USD",
 });
 
-const initialVal = localStorage.getItem('example') //////////////////////////////// LOCAL STORAGE
+// Receives local storage if existing (AKA pulls from local storage)
+const initialVal = localStorage.getItem('initialValStorage') //////////////////////////////// LOCAL STORAGE
+const interest = localStorage.getItem('interestStorage') //////////////////////////////// LOCAL STORAGE
+const lengthOfTime = localStorage.getItem('lengthOfTimeStorage') //////////////////////////////// LOCAL STORAGE
+
+// Sets elements equal to local storage (if local storage empty, then the elements are just empty)
+// Sets HTML to what's in local storage
+// Puts it into the textbox
 document.getElementById('initial-val').value = initialVal; //////////////////////////////// LOCAL STORAGE
+document.getElementById('interest-val').value = interest; //////////////////////////////// LOCAL STORAGE
+document.getElementById('length-of-time').value = lengthOfTime; //////////////////////////////// LOCAL STORAGE
 
 // Receives inputs from the document
 const calculateBtn = document.getElementById('calculateBtn')
@@ -16,9 +25,13 @@ calculateBtn.addEventListener('click', calculateVals)
 
 function calculateVals(){
     const initialVal = document.getElementById('initial-val').value
-    localStorage.setItem('example', initialVal) //////////////////////////////// LOCAL STORAGE
     const interest = document.getElementById('interest-val').value
     const lengthOfTime = document.getElementById('length-of-time').value
+
+    // Sets what you put in the text box as local storage
+    localStorage.setItem('initialValStorage', initialVal) //////////////////////////////// LOCAL STORAGE
+    localStorage.setItem('interestStorage', interest) //////////////////////////////// LOCAL STORAGE
+    localStorage.setItem('lengthOfTimeStorage', lengthOfTime) //////////////////////////////// LOCAL STORAGE
     
     console.log(initialVal)
     console.log(interest)
@@ -35,6 +48,7 @@ function calculateVals(){
 
 // DOM Function
 function outputNums(futureVal, interestEarned){
+    // Takes an element in HTML and equals it to value in JavaScript
     document.getElementById("result-future-value").innerHTML = dollarUS.format(futureVal);
     document.getElementById("result-total-interest").innerHTML = dollarUS.format(interestEarned);
 }
